b3d57f775f72907c06c816ffa513b440a7ac8598
Freddie Tantoco <freddietantoco@Freddies-MacBook-Pro-2.local>
2022-01-20 09:19:18 -0800

    added command line config parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::env;
+use std::fs;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args);
+    
+    // Exit with error
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+}
+
+// Struct does not implement Copy
+// so values inside will "Move"
+struct Config {
+    query: String,
+    filename: String
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+
+        // TODO: Refactor to not use clone
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        let config = Config {
+            query: query,
+            filename: filename
+        };
+
+        Ok(config)
+
+    }
 }
