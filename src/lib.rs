0c8b80599826c9c37a8358c489db1ad1ec017fff
Freddie Tantoco <freddietantoco@Freddies-MacBook-Pro-2.local>
2022-01-23 16:34:47 -0800

    added test

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,22 @@
 use std::error::Error;
 use std::fs;
 
-// Struct does not implement Copy
-// so values inside will "Move"
+#[cfg(test)]
+mod tests {
+    user super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents="\
+        Rust:
+        safe, fast, productive.
+        Pick three.";
+    }
+
+    assert_eq!(vec!["safe", "fast", "productive"], search(query, contents));
+}
+
 pub struct Config {
     pub query: String,
     pub filename: String
